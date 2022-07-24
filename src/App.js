635fcb538952e8ce66945e13fc3830efc961ad2b
João Felipe Rutowitsch Azevedo <104798023+joaofeliperutowitsch@users.noreply.github.com>
2022-07-24 13:42:21 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Meu nome e joão Felipe</h1>
+      </>
+    );
+  }
+}
